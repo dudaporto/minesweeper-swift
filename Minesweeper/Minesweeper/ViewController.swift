4db42f5afbf7608c3f1ebdb007f350da46cb7c15
Maria Eduarda Porto <dudabatistaporto@gmail.com>
2020-08-22 15:46:53 -0300

    Iniciando o projeto

---
+++ Minesweeper/Minesweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by Maria Eduarda Porto on 22/08/20.
+//  Copyright © 2020 Maria Eduarda Porto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
