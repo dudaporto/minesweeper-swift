e3ede4c9f87e2c98964ddc36cc1b1e5a76bc96bc
Maria Eduarda Porto <dudabatistaporto@gmail.com>
2020-08-22 18:34:49 -0300

    Adding Main sotyboard

---
+++ Minesweeper/Minesweeper/Frontend/Screens/Game/Views/BoardView.swift
@@ -0,0 +1,13 @@
+//
+//  BoardView.swift
+//  Minesweeper
+//
+//  Created by Maria Eduarda Porto on 22/08/20.
+//  Copyright © 2020 Maria Eduarda Porto. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+    
+}
---
+++ Minesweeper/Minesweeper/Frontend/Screens/Main/Controllers/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  Minesweeper
+//
+//  Created by Maria Eduarda Porto on 22/08/20.
+//  Copyright © 2020 Maria Eduarda Porto. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+}
