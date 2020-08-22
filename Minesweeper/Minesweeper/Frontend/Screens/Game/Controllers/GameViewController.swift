0505febefc138c371a7d741e5270c5683516ead4
Maria Eduarda Porto <dudabatistaporto@gmail.com>
2020-08-22 19:18:29 -0300

    Criando grid view quadrada

--- Minesweeper/Minesweeper/Frontend/Screens/Game/Controllers/GameViewController.swift
+++ Minesweeper/Minesweeper/Frontend/Screens/Game/Controllers/GameViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var boardView: BoardView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        boardView.setRows(dimention: 10)
     }
 }
 
--- Minesweeper/Minesweeper/Frontend/Screens/Game/Views/BoardView.swift
+++ Minesweeper/Minesweeper/Frontend/Screens/Game/Views/BoardView.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class BoardView: UIView {
+class BoardView: UIStackView {
+    private let squaresSpacing: CGFloat = 3
     
+    private func commonInit() {
+        axis = .vertical
+        spacing = squaresSpacing
+        distribution = .fillEqually
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    func setRows(dimention: Int) {
+        //let rowHeight = frame.height - (CGFloat(dimention - 2) * spacing)
+        
+        for _ in 0..<dimention {
+            let row = UIStackView(arrangedSubviews: [])
+            row.axis = .horizontal
+            row.spacing = squaresSpacing
+            row.distribution = .fillEqually
+            
+            for _ in 0..<dimention {
+                let view = BoardSquareView(frame: .zero)
+                row.addArrangedSubview(view)
+            }
+            
+           addArrangedSubview(row)
+        }
+    }
 }
